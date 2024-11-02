b29702b8713f79695096eb5e922ffb53a627ee5e
haengjun <haengjun.shin@gmail.com>
2024-11-02 16:29:31 +0900

    initialize

---
+++ src/app.js
@@ -0,0 +1,89 @@
+let config, express, axios, fs, app, port, server;
+
+// Node Modoule 로딩
+config = require('./conf/config');
+express = require('express');
+axios = require('axios');
+fs = require('fs');
+
+// Framework 생성
+app = express();
+
+// Request 정의
+app.get('/radio', (req, res) => {
+    let actCd, statNm, chnlNm, useCdn;
+
+    actCd = req.query.action.toUpperCase();         // 액션명
+    statNm = req.query.station.toUpperCase();       // 방송국명
+    chnlNm = req.query.channel.toUpperCase();       // 채널명
+    
+    console.log('\nRequest data - Action: [%s] Station: [%s] Channel: [%s]', actCd, statNm, chnlNm);
+
+    /**
+     * CDN을 사용하지 않는 CBS인 경우에는 환경설정에 저장되어 있는 M3U8주소를 사용하고
+     * 그외 방송국(KBS, MBC, SBS)인 경우 API를 호출하여 CDN의 Stream URL를 얻어낸다
+     */
+    useCdn = config[statNm].useCdn;
+    if (!useCdn) {
+        sendHTML(actCd, res, statNm, chnlNm, config[statNm].url[chnlNm]);
+    } else {
+        let apiUrl;
+
+        apiUrl = config[statNm].url;
+        switch (statNm) {
+            case 'SBS':
+                apiUrl = apiUrl.replace('${chanPt}', config[statNm].channel[chnlNm][0]);
+                apiUrl = apiUrl.replace('${chanCd}', config[statNm].channel[chnlNm][1]);
+                break;
+            default:
+                apiUrl = apiUrl.replaceAll('${chanCd}', config[statNm].channel[chnlNm]);
+        }
+        console.log('Api URL: %s', apiUrl);
+
+        axios
+        .get(apiUrl)
+        .then(function(resp) {
+            /**
+             * 방송사별로 응답객체 구조가 다르므로 분기하여 추출한다
+             */
+            switch (statNm) {
+                case 'KBS':
+                    sendHTML(actCd, res, statNm, chnlNm, resp.data.channel_item[0].service_url);
+                    break;
+                default: 
+                    sendHTML(actCd, res, statNm, chnlNm, resp.data);
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }    
+});
+
+/**
+ * 방송사의 채널별 Streaming URL이 추가된 HTML 반환
+ * 
+ * @param {*} actCd 작업구분
+ * @param {*} res 응답객체
+ * @param {*} statNm 방송국명
+ * @param {*} chnlNm 채널명
+ * @param {*} strmUrl 스트리밍주소
+ */
+function sendHTML(actCd, res, statNm, chnlNm, strmUrl) {
+    console.log('Streaming URL: %s', strmUrl);
+
+    // HTML 전송
+    fs.readFile(__dirname + '/views/' +  (actCd === 'I' ? 'information': 'player') + '.html', 'utf8', (err, html) => {
+        html = html.replace('${brodcastingStationname}', statNm)
+        html = html.replace('${ChannelName}', config.channelName[statNm + '-' + chnlNm])
+        html = html.replace('${streamingUrl}', strmUrl);
+        res.status(200);
+        res.write(html);
+        res.end();
+    });
+}
+
+// 서버 실행
+port = config.listen.port;
+server = app.listen(port);
+console.log('Server listenning on Port %s', port);
---
+++ src/conf/config.js
@@ -0,0 +1,63 @@
+module.exports = {
+    'listen': {
+        'port': 3000
+    },
+    'KBS': {
+        'useCdn': true,
+        'url': 'https://cfpwwwapi.kbs.co.kr/api/v1/landing/live/channel_code/${chanCd}',
+        'channel': {
+            '1RADIO': '21',
+            'CLASSICFM': '24',
+            'COOLFM': '25'
+        }
+    },
+    'MBC': {
+        'useCdn': true,
+        'url': 'https://sminiplay.imbc.com/aacplay.ashx?channel=${chanCd}&protocol=M3U8&agent=webapp',
+        'channel': {
+            'FM': 'sfm',
+            'FM4U': 'mfm',
+            'ALLTHATMUSIC': 'chm'
+        }
+    },
+    'SBS': {
+        'useCdn': true,
+        'url': 'https://apis.sbs.co.kr/play-api/1.0/livestream/${chanPt}/${chanCd}?protocol=hls&ssl=Y',
+        'channel': {
+            'POWERFM': [
+                'powerpc',
+                'powerfm'
+            ],
+            'LOVEFM': [
+                'lovepc',
+                'lovefm'
+            ],
+            'GOLLIRAM': [
+                'sbsdmb',
+                'sbsdmb'
+            ]
+        }
+    },
+    'CBS': {
+        'useCdn': false,
+        'url': {
+            'FM': 'https://m-aac.cbs.co.kr/mweb_cbs981/_definst_/cbs981.stream/chunklist.m3u8',
+            'MUSICFM': 'https://m-aac.cbs.co.kr/mweb_cbs939/_definst_/cbs939.stream/chunklist.m3u8',
+            'JOY4U': 'https://m-aac.cbs.co.kr/mweb_cbscmc/_definst_/cbscmc.stream/chunklist.m3u8'
+        }
+    },
+    "channelName": {
+        'KBS-1RADIO': '1Radio',
+        'KBS-CLASSICFM': 'ClassicFM',
+        'KBS-COOLFM': 'CoolFM',
+        'MBC-FM': '표준FM',
+        'MBC-FM4U': 'FM4U',
+        'MBC-ALLTHATMUSIC': '올댓뮤직',
+        'SBS-POWERFM': '파워FM',
+        'SBS-LOVEFM': '러브FM',
+        'SBS-GOLLIRAM': '고릴라M',
+        'CBS-FM': '표준FM',
+        'CBS-MUSICFM': '음악FM',
+        'CBS-JOY4U': 'JOY4U'
+    }
+};
